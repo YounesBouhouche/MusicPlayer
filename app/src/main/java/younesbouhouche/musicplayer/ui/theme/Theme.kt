f566612a2ff971bff51c81f9e3242024834b264a
Younes Bouhouche <younes.bouhouche12@gmail.com>
2024-07-19 18:51:54 +0100

    Changed font to rubik variable

--- app/src/main/java/younesbouhouche/musicplayer/ui/theme/Theme.kt
+++ app/src/main/java/younesbouhouche/musicplayer/ui/theme/Theme.kt
@@ -3,10 +3,8 @@ package younesbouhouche.musicplayer.ui.theme
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -15,10 +13,7 @@ import androidx.compose.ui.platform.LocalContext
 import younesbouhouche.musicplayer.settings.data.SettingsDataStore
 
 @Composable
-fun AppTheme(
-    colors: List<Color>? = null,
-    content: @Composable () -> Unit,
-) {
+fun AppTheme(content: @Composable () -> Unit) {
     val datastore = SettingsDataStore(LocalContext.current)
     val theme by datastore.theme.collectAsState(initial = "system")
     val isDark =
@@ -27,79 +22,53 @@ fun AppTheme(
             "dark" -> true
             else -> isSystemInDarkTheme()
         }
-    if (colors == null) {
-        val dynamicColor = datastore.dynamicColors.collectAsState(initial = false).value && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
-        val extraDark by datastore.extraDark.collectAsState(initial = false)
-        val colorTheme by datastore.colorTheme.collectAsState(initial = "green")
-        val lightColors =
-            when (colorTheme) {
-                "blue" -> BlueColors.lightScheme
-                "green" -> GreenColors.lightScheme
-                "red" -> RedColors.lightScheme
-                "orange" -> OrangeColors.lightScheme
-                "purple" -> PurpleColors.lightScheme
-                else -> BlueColors.lightScheme
-            }
-        val darkColors =
-            when (colorTheme) {
-                "blue" -> BlueColors.darkScheme
-                "green" -> GreenColors.darkScheme
-                "red" -> RedColors.darkScheme
-                "orange" -> OrangeColors.darkScheme
-                "purple" -> PurpleColors.darkScheme
-                else -> BlueColors.darkScheme
-            }
-        val colorScheme =
-            when {
-                dynamicColor && isDark -> {
-                    if (extraDark) {
-                        dynamicDarkColorScheme(LocalContext.current).copy(
-                            background = Color.Black,
-                            surface = Color.Black,
-                        )
-                    } else {
-                        dynamicDarkColorScheme(LocalContext.current)
-                    }
-                }
-                dynamicColor && !isDark -> {
-                    dynamicLightColorScheme(LocalContext.current)
-                }
-                isDark and extraDark ->
-                    darkColors.copy(
+    val dynamicColor = datastore.dynamicColors.collectAsState(initial = false).value && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
+    val extraDark by datastore.extraDark.collectAsState(initial = false)
+    val colorTheme by datastore.colorTheme.collectAsState(initial = "green")
+    val lightColors =
+        when (colorTheme) {
+            "blue" -> BlueColors.lightScheme
+            "green" -> GreenColors.lightScheme
+            "red" -> RedColors.lightScheme
+            "orange" -> OrangeColors.lightScheme
+            "purple" -> PurpleColors.lightScheme
+            else -> BlueColors.lightScheme
+        }
+    val darkColors =
+        when (colorTheme) {
+            "blue" -> BlueColors.darkScheme
+            "green" -> GreenColors.darkScheme
+            "red" -> RedColors.darkScheme
+            "orange" -> OrangeColors.darkScheme
+            "purple" -> PurpleColors.darkScheme
+            else -> BlueColors.darkScheme
+        }
+    val colorScheme =
+        when {
+            dynamicColor && isDark -> {
+                if (extraDark) {
+                    dynamicDarkColorScheme(LocalContext.current).copy(
                         background = Color.Black,
                         surface = Color.Black,
                     )
-                isDark -> darkColors
-                else -> lightColors
+                } else {
+                    dynamicDarkColorScheme(LocalContext.current)
+                }
             }
-        MaterialTheme(
-            colorScheme = colorScheme,
-            content = content,
-        )
-    } else {
-        val colorScheme =
-            if (isDark) {
-                darkColorScheme(
-                    primary = colors[0],
-                    primaryContainer = colors[2],
-                    onPrimaryContainer = colors[1],
-                    secondary = colors[3],
-                    secondaryContainer = colors[5],
-                    onSecondaryContainer = colors[4],
-                )
-            } else {
-                lightColorScheme(
-                    primary = colors[0],
-                    primaryContainer = colors[1],
-                    onPrimaryContainer = colors[2],
-                    secondary = colors[3],
-                    secondaryContainer = colors[4],
-                    onSecondaryContainer = colors[5],
-                )
+            dynamicColor && !isDark -> {
+                dynamicLightColorScheme(LocalContext.current)
             }
-        MaterialTheme(
-            colorScheme = colorScheme,
-            content = content,
-        )
-    }
+            isDark and extraDark ->
+                darkColors.copy(
+                    background = Color.Black,
+                    surface = Color.Black,
+                )
+            isDark -> darkColors
+            else -> lightColors
+        }
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = rubikTypography(MaterialTheme.typography),
+        content = content,
+    )
 }
