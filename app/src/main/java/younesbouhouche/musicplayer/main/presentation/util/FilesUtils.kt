3149f8016e041e7a5bd61dc526814390b0b7c481
Younes Bouhouche <younes.bouhouche12@gmail.com>
2025-03-27 11:13:30 +0100

    Implemented playlist share feature

--- app/src/main/java/younesbouhouche/musicplayer/main/presentation/util/FilesUtils.kt
+++ app/src/main/java/younesbouhouche/musicplayer/main/presentation/util/FilesUtils.kt
@@ -1,9 +1,11 @@
 package younesbouhouche.musicplayer.main.presentation.util
 
 import android.content.Context
+import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
+import androidx.core.content.FileProvider
 import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
@@ -17,7 +19,7 @@ fun saveUriImageToInternalStorage(
     val bitmap =
         try {
             BitmapFactory.decodeStream(context.contentResolver.openInputStream(uri))
-        } catch (e: IOException) {
+        } catch (_: IOException) {
             null
         }
 
@@ -30,9 +32,36 @@ fun saveUriImageToInternalStorage(
             bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
             outputStream.close()
             file.path
-        } catch (e: IOException) {
+        } catch (_: IOException) {
             file.delete()
             null
         }
     }
+}
+
+fun Context.createTempFile(name: String, content: String): File {
+    val tempFile = File.createTempFile(name, null, cacheDir)
+    FileOutputStream(tempFile).use {
+        it.write(content.toByteArray())
+    }
+    return tempFile
+}
+
+fun Context.shareFile(file: File, fileType: String) {
+    val fileUri = FileProvider.getUriForFile(
+        this,
+        "${packageName}.fileprovider",
+        file
+    )
+    startActivity(
+        Intent.createChooser(
+            Intent(Intent.ACTION_SEND).apply {
+                putExtra(Intent.EXTRA_STREAM, fileUri)
+                putExtra(Intent.EXTRA_TEXT, file.name)
+                addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                type = fileType
+            },
+            "Share file",
+        ),
+    )
 }
