e1aaf98301e30e5cc48ca8db380cbae84482246c
Younes Bouhouche <younes.bouhouche12@gmail.com>
2025-03-30 08:54:25 +0100

    Added artists repo

--- app/src/main/java/younesbouhouche/musicplayer/di/AppModule.kt
+++ app/src/main/java/younesbouhouche/musicplayer/di/AppModule.kt
@@ -4,14 +4,19 @@ import android.content.Context
 import android.media.AudioManager
 import android.media.MediaMetadataRetriever
 import androidx.room.Room
+import io.ktor.client.engine.cio.CIO
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
+import younesbouhouche.musicplayer.main.data.ArtistsRepoImpl
 import younesbouhouche.musicplayer.main.data.FilesRepoImpl
 import younesbouhouche.musicplayer.main.data.PlayerDataStore
 import younesbouhouche.musicplayer.main.data.PlayerRepoImpl
+import younesbouhouche.musicplayer.main.data.ThumbnailCache
 import younesbouhouche.musicplayer.main.data.dao.AppDao
 import younesbouhouche.musicplayer.main.data.db.AppDatabase
+import younesbouhouche.musicplayer.main.data.networking.HttpClientFactory
+import younesbouhouche.musicplayer.main.domain.repo.ArtistsRepo
 import younesbouhouche.musicplayer.main.domain.repo.FilesRepo
 import younesbouhouche.musicplayer.main.domain.repo.PlayerRepo
 import younesbouhouche.musicplayer.main.presentation.viewmodel.MainVM
@@ -20,7 +25,9 @@ import younesbouhouche.musicplayer.settings.data.SettingsDataStore
 val appModule = module {
     viewModelOf(::MainVM)
     single<AppDatabase> {
-        Room.databaseBuilder(androidContext(), AppDatabase::class.java, "contacts.db").build()
+        Room
+            .databaseBuilder(androidContext(), AppDatabase::class.java, "files.db")
+            .build()
     }
 
     single<AppDao> {
@@ -44,12 +51,23 @@ val appModule = module {
         )
     }
 
+    single<ThumbnailCache> {
+        ThumbnailCache(androidContext())
+    }
+
+    single { HttpClientFactory.create(CIO.create()) }
+
+    single<ArtistsRepo> {
+        ArtistsRepoImpl(get())
+    }
+
     single<FilesRepo> {
         FilesRepoImpl(
             androidContext(),
             get(),
             get(),
-            get()
+            get(),
+            get(),
         )
     }
 }
