956733d2ea7c13471357c3b16cbb2ed4f2acedba
Younes Bouhouche <younes.bouhouche12@gmail.com>
2025-04-02 15:15:51 +0100

    Added global image loader for Coil

--- app/src/main/java/younesbouhouche/musicplayer/di/App.kt
+++ app/src/main/java/younesbouhouche/musicplayer/di/App.kt
@@ -1,15 +1,42 @@
 package younesbouhouche.musicplayer.di
 
 import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.disk.DiskCache
+import coil.memory.MemoryCache
+import coil.request.CachePolicy
+import coil.util.DebugLogger
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
-class App : Application() {
+class App : Application(), ImageLoaderFactory {
     override fun onCreate() {
         super.onCreate()
         startKoin {
             androidContext(this@App)
             modules(appModule, dialogModule)
         }
+    }
+
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader.Builder(this)
+            .memoryCachePolicy(CachePolicy.ENABLED)
+            .memoryCache {
+                MemoryCache.Builder(this)
+                    .maxSizePercent(0.25)
+                    .strongReferencesEnabled(true)
+                    .build()
+            }
+            .diskCachePolicy(CachePolicy.ENABLED)
+            .diskCache {
+                DiskCache.Builder()
+                    .maxSizePercent(.03)
+                    .directory(cacheDir)
+                    .build()
+            }
+            .logger(DebugLogger())
+            .crossfade(true)
+            .build()
     }
 }
