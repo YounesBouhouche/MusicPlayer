103f6f1c9e1a6cc0ee5106d910cf84904c7bf1ec
Younes <younes.bouhouche12@gmail.com>
2025-12-15 14:04:19 +0100

    fixed: Enhance TimerType with tomorrow handling and target date methods

--- app/src/main/java/younesbouhouche/musicplayer/features/main/domain/events/TimerType.kt
+++ app/src/main/java/younesbouhouche/musicplayer/features/main/domain/events/TimerType.kt
@@ -1,11 +1,39 @@
 package younesbouhouche.musicplayer.features.main.domain.events
 
+import java.time.LocalDateTime
+
 sealed interface TimerType {
     data object Disabled : TimerType
 
     data class Duration(val ms: Long) : TimerType
 
-    data class Time(val hour: Int, val min: Int) : TimerType
+    data class Time(val hour: Int, val min: Int, val tomorrow: Boolean) : TimerType {
+        constructor(hour: Int, min: Int): this(
+            hour,
+            min,
+            LocalDateTime.now().let { now ->
+                now.withSecond(0)
+                    .withNano(0)
+                    .withHour(hour)
+                    .withMinute(min)
+                    .isBefore(now)
+            }
+        )
+        fun getTargetDate(): LocalDateTime {
+            return LocalDateTime.now()
+                .withHour(hour)
+                .withMinute(min)
+                .withSecond(0)
+                .withNano(0)
+                .plusDays(if (tomorrow) 1 else 0)
+        }
+        fun getTargetDateMillis(): Long {
+            return getTargetDate()
+                .atZone(java.time.ZoneId.systemDefault())
+                .toInstant()
+                .toEpochMilli()
+        }
+    }
 
     data class End(val tracks: Int) : TimerType
 }
