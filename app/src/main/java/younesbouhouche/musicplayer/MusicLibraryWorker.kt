501367b08ecabbbe0bca8b12d904d47807e365b9
Younes <younes.bouhouche12@gmail.com>
2026-03-16 11:09:02 +0100

    Migrated to work manager to handle media fetching

---
+++ app/src/main/java/younesbouhouche/musicplayer/MusicLibraryWorker.kt
@@ -0,0 +1,36 @@
+package younesbouhouche.musicplayer
+
+import android.content.Context
+import androidx.work.BackoffPolicy
+import androidx.work.CoroutineWorker
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import androidx.work.WorkerParameters
+import younesbouhouche.musicplayer.core.domain.repositories.MusicRepository
+import java.time.Duration
+
+class MusicLibraryWorker(
+    val context: Context,
+    workerParams: WorkerParameters,
+    val musicRepository: MusicRepository
+): CoroutineWorker(context, workerParams) {
+    override suspend fun doWork(): Result {
+        return try {
+            musicRepository.refreshMusicLibrary()
+            Result.success()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Result.failure()
+        }
+    }
+}
+
+fun launchWorkRequest(context: Context) {
+    val worker = OneTimeWorkRequestBuilder<MusicLibraryWorker>()
+        .setBackoffCriteria(
+            backoffPolicy = BackoffPolicy.EXPONENTIAL,
+            duration = Duration.ofSeconds(10),
+        )
+        .build()
+    WorkManager.getInstance(context).enqueue(worker)
+}
