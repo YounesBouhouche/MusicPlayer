8e6f645737f2fa79b7c95c8d3eb660e4531c7054
Younes <younes.bouhouche12@gmail.com>
2026-03-18 12:15:23 +0100

    Updated dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -166,5 +166,6 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.navigation3)
     implementation(libs.androidx.core.splashscreen)
     implementation(libs.androidx.work.runtime.ktx)
+    implementation(libs.dotsindicator)
 }
 
